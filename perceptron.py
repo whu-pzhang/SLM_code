366faac530c84af9d7aa06de53fa3a8d4b902276
whu-pzhang <pzhang@whu.edu.cn>
2018-12-28 08:25:21 +0800

    implenment Dual-Perceptron

--- perceptron.py
+++ perceptron.py
@@ -43,7 +43,38 @@ class Perceptron(object):
         y_pred = np.sign(X @ self.W + self.b)
         return y_pred
 
-    def loss(self, X, y):
+
+class DualPerceptron(object):
+    """
+    Dual form of Perceptron
+    """
+
+    def __init__(self):
+        self.alpha = None
+        self.b = None
+
+    def train(self, X, y, learning_rate=1., verbose=False):
+        """
+        train the classifier
+        """
+        X = np.asarray(X)
+        N, D = X.shape
+        y = np.asarray(y).reshape(N, 1)
+        assert N == y.shape[0], 'labels not match!'
+        # init
+        self.alpha = np.zeros((N, 1), dtype=X.dtype)
+        self.b = 0
+
+        gram_matrix = X @ X.T
+
+        while True:
+            index = np.random.choice(N, 1)
+            y_pred = alpha @ y * gram_matrix[:, index] + self.b
+            if y[index] * y_pred <= 0:
+                self.alpha[index] += learning_rate
+                self.b += learning_rate * y[index]
+
+    def predict(self, X):
         pass
 
 
